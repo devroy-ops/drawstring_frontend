364ecc98e5155ca07b04828fa5d82f0313d6e04c
uma-victor1 <umavictor11@gmail.com>
2022-04-15 18:31:11 +0100

    changes

--- src/services/utils.js
+++ src/services/utils.js
@@ -33,8 +33,8 @@ export async function initContracts() {
 
     };
   const near = await connect({ deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() }, ...nearConfig });
-  const account = await near.account(currentUser.accountId);
-  const balance = await account.getAccountBalance()
+  // const account = await near.account(currentUser.accountId);
+  // const balance = await account.getAccountBalance()
    available = formatNearAmount(balance.available);
   } else {
     currentUser = {
